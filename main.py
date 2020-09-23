7e7d689833c7dfbed039a6546a2d497b793be07d
Olivier <mero1993@gmail.com>
2020-09-23 16:33:29 -0400

    adding texts

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import trainer as t
+
+#path C:\Users\mero1\Documents\Fall 2020\B51\b51-tp1\textes\
+
 def main():
     pass
-    #UI LOOP
+    # UI LOOP
+
 
 if __name__ == '__main__':
     main()
--- reader.py
+++ reader.py
@@ -2,7 +2,7 @@ import numpy as np
 
 
 class Reader:
-    def __init__(self, widow_size, path):  # return ndarray
+    def __init__(self, widow_size, *paths):  # return ndarray
         return self.array
 
     def read(self):
