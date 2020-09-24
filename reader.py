ddf3320e5ab1cab6e118aa1426f8001b1e5c8b96
Olivier <mero1993@gmail.com>
2020-09-24 11:13:48 -0400

    rollback

--- reader.py
+++ reader.py
@@ -11,8 +11,8 @@ class Reader:
         self.text_list = list()
         self.read()
         self.index()
-        self.result_array = self.match()
-        print(self.result_array)
+        self.result_array = self.build_array()
+        print('fini')
 
     def read(self):
         for path in self.paths:
@@ -26,23 +26,10 @@ class Reader:
                 if word not in self.word_index.keys():
                     self.word_index[word] = current_index
                     current_index += 1
-        print(self.word_index)
 
-    def match(self):
-        result_array = np.zeros((len(self.word_index), len(self.word_index)))
-        for text in self.text_list:
-            for word in self.word_index.keys():
-                regex = '((?:[a-zA-ZÀ-ÿ]*[.!?,\'\";:\s]*){' + str(self.widow_size) + \
-                        '})[.!?,\'\";:\s]*' + word + '[.!?,\'\";:\s]*((?:[a-zA-ZÀ-ÿ]*[.!?,\'\";:\s]*){' + \
-                        str(self.widow_size) + '})'
-                match_result = re.findall(regex, text)
-                if match_result:
-                    for result in match_result:
-                        for result_group in result:
-                            for result_word in filter(None, re.split('[\-.,\'\";:«»!?\s]', result_group)):
-                                result_array[self.word_index.get(word)][self.word_index.get(result_word)] += 1
-        return result_array
+    def build_array(self):
+        pass
 
 
 if __name__ == '__main__':
-    Reader(5, 'utf-8', ['test.txt'])
+    Reader(5, 'utf-8', ['LesTroisMousquetairesUTF8.txt'])
