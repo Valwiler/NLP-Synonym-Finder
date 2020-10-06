d996867a7d002c67fb09047edb87e30ff7c748eb
Olivier <mero1993@gmail.com>
2020-10-06 14:59:22 -0400

    Chrono v3 (v2 never worked)

--- chrono.py
+++ chrono.py
@@ -2,19 +2,29 @@ import time
 
 
 class Chrono:
-
     def __init__(self):
         self.start_stamp = 0
         self.logs = list()
 
+    # starts timer
     def start(self):
         self.start_stamp = time.time()
 
+    # clock time and message
+    def log(self, message):
+        self.logs.append(Log(message))
+
+    # clock endtime and pring clock list
     def end(self):
-        self.logs.append(time.time())
-        for i,timestamp in enumerate(self.logs):
-            print("#"+str(i) + " :" + str(self.start_stamp - timestamp))
+        self.logs.append(Log('Fin'))
+        for log in self.logs:
+            log.print_log(self.start_stamp)
 
-    def log(self):
-        self.logs.append(time.time())
 
+class Log:
+    def __init__(self, message):
+        self.message = message
+        self.timestamp = time.time()
+
+    def print_log(self, start_time):
+        print(self.message + " : " + str(self.timestamp - start_time))
