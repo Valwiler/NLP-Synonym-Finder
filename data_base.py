0926cb7c590c35d48c45244105c98738ac78dffb
Valwiler <tymotheilanouette@gmail.com>
2020-11-10 11:58:41 -0500

    "Completer les fonctions de bases necessaire a l'acces a la BD"

--- data_base.py
+++ data_base.py
@@ -14,10 +14,16 @@ CREATE_COOCURENCE_TABLE = ' CREATE TABLE (?) IF NOT EXISTS  (' \
                           'occurences INT NOT NULL,' \
                           'FOREIGN KEY (id_word) REFERENCES vocabulary_table(id),' \
                           'FOREIGN KEY (id_adjacent_word) REFERENCES vocabulary_table(id))'
+CREATE_STOP_LIST = 'CREATE TABLE stop_word_table IF NOT EXISTS ('\
+                   'id INT NOT NULL ,  '\
+                   'stop_word char(50) NOT NULL, '\
+                   'FOREIGN KEY (id_stop_word) REFERENCES vocabulary_table(id)) '
 
 INSERT_NEW_WORD = 'INSERT INTO vocabulary_table VALUES( ? ) IF NOT EXISTS'
 INSERT_NEW_OCCURENCE = 'INSERT INTO (?) VALUES ( ?, ? , ? ) IF NOT EXISTS'  # toujouts penser a initialiser le nombre d'occurence a 0
 UPDATE_OCCURENCE = 'UPDATE (?) SET occurences = +1 WHERE id_word = (?) AND id_adjacent_word = (?)'
+
+INSERT_STOP_LIST = 'INSERT INTO stop_word_table VALUES( ? ) IF NOT EXISTS '
 
 
 class Data_Base:
@@ -27,10 +33,14 @@ class Data_Base:
         self.cursor = self.connexion.cursor()
         self.cursor.execute('PRAGMA foreign_keys = 1')
 
-    def get_word_index(self):
-        pass
-        # retourne l'idex du mot
 
-    def add_word(self):
-        pass
-        # ajoute le mot
+
+
+    def get_word_index(self, word, table):
+        self.cursor.execute(GET_WORD_INDEX, (table, word))
+        return self.cursor.fetchall()
+
+
+    def add_word(self, word):
+        self.cursor.execute(INSERT_NEW_WORD, word)
+
